72b8efd54dc4aec28d78e7887cfeac55241edeb5
nicolecht <nicolecht@hotmail.com>
2022-10-20 18:18:21 +0800

    search-box and card-list functions

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CardList;
